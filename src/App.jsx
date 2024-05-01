aff5b304450485b97455e64e22159327b41fa270
unknown <tejaswinihegde25@gmail.com>
2024-05-01 21:31:46 +0530

    React Project file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Hero from './Hero'
+import Navigation from './Navigation'
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Hero />
+    </>
+  )
+}
+
+export default App
